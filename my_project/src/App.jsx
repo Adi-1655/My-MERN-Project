21a69043b0db90b9ebe40c488fcc99724ffb3837
Adi-1655 <adityadeshmukh1085@gmail.com>
2025-10-15 22:41:19 +0530

    Remove duplicate FontAwesome import in App.jsx

--- my_project/src/App.jsx
+++ my_project/src/App.jsx
@@ -1,23 +1,30 @@
 // src/App.jsx
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+// Import Your Pages
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ProductsPage from "./pages/ProductsPage";
 import MaterialsPage from "./pages/MaterialsPage";
 import GalleryPage from "./pages/GalleryPage";
 import ContactPage from "./pages/ContactPage";
+
+// Import Your Components
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import SocialMediaIcons from "./components/SocialMediaIcons";
-import "@fortawesome/fontawesome-free/css/all.min.css"; // ✅ ensure this line is here
 
 function App() {
   return (
     <Router>
+      {/* These components will show on every page */}
       <Header />
       <Navbar />
 
+      {/* This section will switch between your pages */}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
@@ -27,10 +34,10 @@ function App() {
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
 
+      {/* These components will also show on every page */}
       <div style={{ marginTop: "40px" }}>
         <SocialMediaIcons />
       </div>
-
       <Footer />
     </Router>
   );
